4469073af0826ffbba92a5621b5de84a04c804d6
Kishan Patel <mail@kishanpatel.dev>
2023-07-01 20:37:09 -0400

    Show recipe names

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import render_template
 
 import config
-from models import *
+from config import db
+from models import Recipe
 
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    recipes = db.session.execute(
+        db.select(Recipe)
+    ).scalars().unique().all()
+    
+    return render_template("index.html", recipes=recipes)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
