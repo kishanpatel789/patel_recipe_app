275caaa6dcd79096f3552451ec798f29a86c1982
Kishan Patel <mail@kishanpatel.dev>
2023-07-03 20:22:36 -0400

    Attempt create recipe

--- config.py
+++ config.py
@@ -5,11 +5,16 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = pathlib.Path(__file__).parent.resolve()
-connex_app = connexion.App(__name__, specification_dir=basedir)
+# connex_app = connexion.App(__name__, specification_dir=basedir)
+connex_app = connexion.FlaskApp(__name__, specification_dir=basedir)
+# connex_app.add_api(basedir / "swagger.yml")
+
 
 app = connex_app.app
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'recipe.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config["DEBUG"] = True
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
--- recipe.py
+++ recipe.py
@@ -19,3 +19,19 @@ def read_one(recipe_id):
         return recipe_schema.dump(recipe)
     else:
         abort(404, f"Recipe {recipe_id} not found")
+
+def create(recipe):
+    name = recipe.get("name")
+    existing_recipe = db.session.execute(
+        db.select(Recipe).filter(
+            Recipe.name==name
+        )
+    ).scalars().unique().one_or_none()
+    
+    if existing_recipe is None:
+        new_recipe = recipe_schema.load(recipe, session=db.session)
+        db.session.add(new_recipe)
+        db.session.commit()
+        return recipe_schema.dump(new_recipe), 201
+    else:
+        abort(406, f"Recipe with name {name} already exists")
