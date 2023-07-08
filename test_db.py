765f03aa9904010a1d24d123199d67a3802bbde8
Kishan Patel <mail@kishanpatel.dev>
2023-07-08 14:32:31 -0400

    Start bootstrap implementation

--- test_db.py
+++ test_db.py
@@ -1,23 +1,25 @@
 # %%
 
 from application import create_app
-from application.models import db, Recipe, Ingredient, Direction, Unit
+from application.models import db, Recipe, Ingredient, Direction, Unit, Tag
 
 # %%
 app = create_app()
 
 # %%
 with app.app_context():
-    rep = db.session.execute(
-        db.select(Recipe).where(Recipe.id==1)
-    ).scalars().unique().one_or_none()
+    # rep = db.session.execute(
+    #     db.select(Recipe).where(Recipe.id==1)
+    # ).scalars().unique().one_or_none()
 
-    comp = rep.complementary_dishes.all()
+    # comp = rep.complementary_dishes.all()
 
-    ingredients = db.session.execute(
-        db.select(Ingredient).where(Ingredient.recipe_id==1)
-    ).scalars().unique().all()
+    # ingredients = db.session.execute(
+    #     db.select(Ingredient).where(Ingredient.recipe_id==1)
+    # ).scalars().unique().all()
 
-    # comp_dish = db.
+    new_tag = Tag(name='hello')
+    t1 = db.session.add(new_tag)
+    t2 = db.session.commit()
 
 # %%
