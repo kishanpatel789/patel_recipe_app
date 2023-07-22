7a977da7c5e8cec8966891367ec9a534fb477ff6
Kishan Patel <mail@kishanpatel.dev>
2023-07-21 20:12:41 -0400

    Present units in frontend table

--- application/routes.py
+++ application/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, make_response, abort, \
     redirect, url_for, flash
 from flask import current_app as app
 
-from .models import db, Recipe, Ingredient, Direction, Tag
-from .forms import TagForm
+from .models import db, Recipe, Ingredient, Direction, Tag, Unit
+from .forms import TagForm, UnitForm
 
 # register custom jinja filters
 def format_number(value):
@@ -48,7 +48,7 @@ def show_recipe(recipe_id):
         abort(404)
 
 # tag
-@app.route('/tag/', methods=['GET', 'POST'])
+@app.route('/tag/', methods=['GET'])
 def show_tags():
     # query database
     tags = db.session.execute(
@@ -107,7 +107,7 @@ def create_tag():
 
     return redirect(url_for("show_tags"))
         
-@app.route('/tag/edit/<int:tag_id>', methods=['GET', 'POST'])
+@app.route('/tag/edit/<int:tag_id>', methods=['POST'])
 def edit_tag(tag_id):
     # look up tag_id
     existing_tag = db.session.execute(
@@ -128,11 +128,6 @@ def edit_tag(tag_id):
                 db.session.commit()      
     return redirect(url_for("show_tags"))
 
-    # return render_template(
-    #     "edit_tag.html",
-    #     form=form,
-    # )
-
 
 @app.route('/tag/delete/<int:tag_id>', methods=['GET'])
 def delete_tag(tag_id):
@@ -148,6 +143,21 @@ def delete_tag(tag_id):
 
     return redirect(url_for("show_tags"))
 
+# unit
+@app.route('/unit/', methods=['GET'])
+def show_units():
+    # query database
+    units = db.session.execute(
+        db.select(Unit)
+    ).scalars().all()
+
+    form = TagForm()
+
+    return render_template(
+        'unit.html', 
+        units=units, 
+        form=form,
+    )
 
 @app.errorhandler(404)
 def page_not_found(error):
--- config.py
+++ config.py
@@ -8,6 +8,7 @@ load_dotenv(basedir / ".env")
 
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir / 'recipe.db'}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO=True
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
