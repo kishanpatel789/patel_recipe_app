fb8982afeb8bb21f67a6bb06e0c9f2727d055432
Kishan Patel <mail@kishanpatel.dev>
2023-08-31 16:50:59 -0400

    Update create recipe form for new format

--- application/routes.py
+++ application/routes.py
@@ -69,53 +69,58 @@ def create_recipe():
 
     form_ingredient_template.unit_id.choices = [(-1, '')] + [(u.id, u.name) for u in units]
     
+    if form.validate_on_submit():
+        # check for existing recipe with name
+        existing_recipe = db.session.execute(
+            db.select(Recipe).where(Recipe.name==form.name.data)
+        ).scalars().unique().one_or_none()
+
+        if existing_recipe:
+            flash(f"Recipe with '{form.name.data}' already exists")
+        else:
+            # update recipe model
+            recipe_orm = Recipe(
+                name=form.name.data,
+                created_by = 'kishan'  # TEMP UPDATE; re-do later with logged in user
+            )
+            db.session.add(recipe_orm)
+            db.session.commit()
+            db.session.refresh(recipe_orm)
+
+            # update direction model
+            for i, direction in enumerate(form.directions):
+                direction_orm = Direction(
+                    recipe_id = recipe_orm.id,
+                    order_id = i+1,
+                    description = direction.description_.data,
+                )
+                db.session.add(direction_orm)
+                db.session.commit()
+                db.session.refresh(direction_orm)
+
+                # update ingredient model
+                for j, ingredient in enumerate(direction.ingredients):
+                    ingredient_orm = Ingredient(
+                        direction_id = direction_orm.id,
+                        order_id = j+1,
+                        quantity = ingredient.quantity.data,
+                        unit_id = ingredient.unit_id.data,
+                        item = ingredient.item.data,
+                    )
+                    db.session.add(ingredient_orm)
+
+            db.session.commit()
+
+            return redirect(url_for('home'))
     
+    if request.method == 'POST' and form.errors:
+        # return form.errors
+        for field, errors in form.errors.items():
+                for error in errors:
+                    flash(f"{field}: {error}", "error")
 
-    # if form.validate_on_submit():
-    #     # check for existing recipe with name
-    #     existing_recipe = db.session.execute(
-    #         db.select(Recipe).where(Recipe.name==form.name.data)
-    #     ).scalars().unique().one_or_none()
-
-    #     if existing_recipe:
-    #         flash(f"Recipe with '{form.name.data}' already exists")
-    #     else:
-
-    #         # update recipe model
-    #         recipe_orm = Recipe(
-    #             name=form.name.data,
-    #             created_by = 'kishan'  # TEMP UPDATE; re-do later with logged in user
-    #         )
-    #         db.session.add(recipe_orm)
-    #         db.session.commit()
-    #         db.session.refresh(recipe_orm)
-
-    #         # update ingredient model
-    #         for i, ingredient in enumerate(form.ingredients):
-    #             ingredient_orm = Ingredient(
-    #                 recipe_id = recipe_orm.id,
-    #                 order_id = i+1,
-    #                 quantity = ingredient.quantity.data,
-    #                 unit_id = ingredient.unit_id.data,
-    #                 item = ingredient.item.data,
-    #             )
-    #             db.session.add(ingredient_orm)
-    #         db.session.commit()
-
-    #         # update direction model
-
-
-
-    #         return redirect(url_for('home'))
-    
-    # if request.method == 'POST' and form.errors:
-    #     return form.errors
-    #     # for field, errors in form.errors.items():
-    #     #         for error in errors:
-    #     #             flash(f"{field}: {error}", "error")
-
-    if request.method == 'POST':
-        return form.data
+    # if request.method == 'POST':
+    #     return form.data
     return render_template(
         'create_recipe.html',
         form=form,
