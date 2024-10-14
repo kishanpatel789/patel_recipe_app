8ebb8141ed6e1450c591627c4afb06ae96ee841d
Kishan Patel <mail@kishanpatel.dev>
2024-10-14 16:20:56 -0500

    Format tags router

--- api/routers/tags.py
+++ api/routers/tags.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 
 from sqlalchemy import select
 from sqlalchemy.orm import Session  # for typing
-from sqlalchemy.sql.selectable import Select # for typing
+from sqlalchemy.sql.selectable import Select  # for typing
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from typing import Type
 
@@ -10,119 +10,138 @@ from .. import models, schemas
 from ..database import get_db
 
 router = APIRouter(
-  prefix="/tags",
-  tags=["tags"],
+    prefix="/tags",
+    tags=["tags"],
 )
 
+
 # helper functions
-def modify_query_for_activity(model: Type[DeclarativeMeta], query: Select, active_only: bool):
-  if active_only:
-    return query.where(model.is_active==True)
-  else:
-    return query
+def modify_query_for_activity(
+    model: Type[DeclarativeMeta], query: Select, active_only: bool
+):
+    if active_only:
+        return query.where(model.is_active == True)
+    else:
+        return query
 
 
 # tag endpoints
 @router.get("/", response_model=list[schemas.TagSchema])
 def read_tags(active_only: bool = False, db: Session = Depends(get_db)):
 
-  base_query = select(models.Tag).order_by(models.Tag.name)
-  finished_query = modify_query_for_activity(models.Tag, base_query, active_only)
+    base_query = select(models.Tag).order_by(models.Tag.name)
+    finished_query = modify_query_for_activity(models.Tag, base_query, active_only)
 
-  tag_orms = db.execute(
-    finished_query
-  ).scalars().unique().all()
+    tag_orms = db.execute(finished_query).scalars().unique().all()
 
-  return tag_orms
+    return tag_orms
+
 
 @router.get("/{id}", response_model=schemas.TagSchema)
 def read_tag(id: int, active_only: bool = False, db: Session = Depends(get_db)):
 
-  base_query = select(models.Tag).where(models.Tag.id==id)
-  finished_query = modify_query_for_activity(models.Tag, base_query, active_only)
+    base_query = select(models.Tag).where(models.Tag.id == id)
+    finished_query = modify_query_for_activity(models.Tag, base_query, active_only)
 
+    tag_orm = db.execute(finished_query).unique().scalar_one_or_none()
+    if not tag_orm:
+        raise HTTPException(status_code=404, detail=f"Tag '{id}' not found")
 
-  tag_orm = db.execute(
-    finished_query
-  ).unique().scalar_one_or_none()
-  if not tag_orm:
-    raise HTTPException(status_code=404, detail=f"Tag '{id}' not found")
-  
-  return tag_orm
+    return tag_orm
+
 
 @router.post("/", response_model=schemas.TagSchema, status_code=201)
 def create_tag(tag_schema_input: schemas.TagCreate, db: Session = Depends(get_db)):
-  
-  # check for existing tag
-  existing_tag = db.execute(
-    select(models.Tag).where(
-      models.Tag.name==tag_schema_input.name
+
+    # check for existing tag
+    existing_tag = (
+        db.execute(select(models.Tag).where(models.Tag.name == tag_schema_input.name))
+        .unique()
+        .scalar_one_or_none()
     )
-  ).unique().scalar_one_or_none()
-  if existing_tag:
-    raise HTTPException(status_code=409, detail=f"Tag '{tag_schema_input.name}' with id '{existing_tag.id}' already exists")
-  
-  # create model instance
-  tag_orm = models.Tag(**tag_schema_input.model_dump())
+    if existing_tag:
+        raise HTTPException(
+            status_code=409,
+            detail=f"Tag '{tag_schema_input.name}' with id '{existing_tag.id}' already exists",
+        )
 
-  # update db
-  db.add(tag_orm)
-  db.commit()
-  db.refresh(tag_orm)
+    # create model instance
+    tag_orm = models.Tag(**tag_schema_input.model_dump())
 
-  return tag_orm
+    # update db
+    db.add(tag_orm)
+    db.commit()
+    db.refresh(tag_orm)
+
+    return tag_orm
+
 
 @router.put("/{id}", response_model=schemas.TagSchema)
-def update_tag(id: int, tag_schema_input: schemas.TagEdit, db: Session = Depends(get_db)):
-  
-  # check for existing tag
-  existing_tag = db.execute(
-    select(models.Tag).where(models.Tag.id==id)
-  ).unique().scalar_one_or_none()
-  if not existing_tag:
-    raise HTTPException(status_code=404, detail=f"Tag '{id}' does not exist")
-  
-  # check input schema tag name doesn't already exist on another record
-  if existing_tag.name != tag_schema_input.name:
-    conflicting_tag = db.execute(
-      select(models.Tag).where(
-        models.Tag.name==tag_schema_input.name
-      )
-    ).unique().scalar_one_or_none()
-    if conflicting_tag:
-      raise HTTPException(status_code=400, detail=f"Tag '{tag_schema_input.name}' with id '{conflicting_tag.id}' already exists. Cannot update tag '{id}'.")
+def update_tag(
+    id: int, tag_schema_input: schemas.TagEdit, db: Session = Depends(get_db)
+):
 
-  # # create model instance
-  # tag_orm_new = models.Tag(id=id, **tag_schema_input.model_dump())
+    # check for existing tag
+    existing_tag = (
+        db.execute(select(models.Tag).where(models.Tag.id == id))
+        .unique()
+        .scalar_one_or_none()
+    )
+    if not existing_tag:
+        raise HTTPException(status_code=404, detail=f"Tag '{id}' does not exist")
 
-  # # update attributes on existing tag
-  # for key in tag_orm_new.__mapper__.attrs.keys():
-  #   setattr(existing_tag, key, getattr(tag_orm_new, key))
-  for key, value in tag_schema_input.model_dump().items():
-    setattr(existing_tag, key, value)
+    # check input schema tag name doesn't already exist on another record
+    if existing_tag.name != tag_schema_input.name:
+        conflicting_tag = (
+            db.execute(
+                select(models.Tag).where(models.Tag.name == tag_schema_input.name)
+            )
+            .unique()
+            .scalar_one_or_none()
+        )
+        if conflicting_tag:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Tag '{tag_schema_input.name}' with id '{conflicting_tag.id}' already exists. Cannot update tag '{id}'.",
+            )
 
-  # update db
-  db.commit()
-  db.refresh(existing_tag)
+    # # create model instance
+    # tag_orm_new = models.Tag(id=id, **tag_schema_input.model_dump())
 
-  return existing_tag
+    # # update attributes on existing tag
+    # for key in tag_orm_new.__mapper__.attrs.keys():
+    #   setattr(existing_tag, key, getattr(tag_orm_new, key))
+    for key, value in tag_schema_input.model_dump().items():
+        setattr(existing_tag, key, value)
+
+    # update db
+    db.commit()
+    db.refresh(existing_tag)
+
+    return existing_tag
+
 
 @router.delete("/{id}", response_model=schemas.TagSchema)
 def delete_tag(id: int, db: Session = Depends(get_db)):
-  
-  # check for existing tag
-  existing_tag = db.execute(
-    select(models.Tag).where(models.Tag.is_active==True).where(models.Tag.id==id)
-  ).unique().scalar_one_or_none()
-  if not existing_tag:
-    raise HTTPException(status_code=404, detail=f"Tag '{id}' does not exist")
 
-  # make existing tag inactive
-  existing_tag.is_active = False
+    # check for existing tag
+    existing_tag = (
+        db.execute(
+            select(models.Tag)
+            .where(models.Tag.is_active == True)
+            .where(models.Tag.id == id)
+        )
+        .unique()
+        .scalar_one_or_none()
+    )
+    if not existing_tag:
+        raise HTTPException(status_code=404, detail=f"Tag '{id}' does not exist")
 
-  # update db
-  db.commit()
-  db.refresh(existing_tag)
+    # make existing tag inactive
+    existing_tag.is_active = False
 
-  return existing_tag
+    # update db
+    db.commit()
+    db.refresh(existing_tag)
 
+    return existing_tag
