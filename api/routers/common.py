3ed1a79019a593f04a8857a8d309160eb0321603
Kishan Patel <mail@kishanpatel.dev>
2024-11-18 11:54:42 -0600

    Resolve typing issue for common func

--- api/routers/common.py
+++ api/routers/common.py
@@ -1,11 +1,11 @@
 from sqlalchemy.sql.selectable import Select  # for typing
-from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.orm import DeclarativeBase
 from typing import Type
 
 
 # helper functions
 def modify_query_for_activity(
-    model: Type[DeclarativeMeta], query: Select, active_only: bool
+    model: Type[DeclarativeBase], query: Select, active_only: bool
 ):
     if active_only:
         return query.where(model.is_active == True)
