42357ebc059315fe8976e51849ac4e8b5eef8ada
Kishan Patel <mail@kishanpatel.dev>
2024-11-21 13:56:20 -0600

    Add tag delete tests

--- api/tests/test_tags.py
+++ api/tests/test_tags.py
@@ -9,3 +9,22 @@ def test_create_tag(test_db, test_client):
     data = response.json()
     assert data["name"] == "Vegetarian"
 
+def test_delete_tag(test_db, test_client):
+    response = test_client.delete(
+        "/tags/4",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data['is_active'] == False
+
+def test_delete_tag_nonexistent_id(test_db, test_client):
+    response = test_client.delete(
+        "/tags/40000",
+    )
+    assert response.status_code == 404
+
+def test_delete_tag_invalid_id(test_db, test_client):
+    response = test_client.delete(
+        "/tags/blah",
+    )
+    assert response.status_code == 422
