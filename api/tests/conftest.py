ce78703c2f0d44405a70de36be14551da2b37467
Kishan Patel <mail@kishanpatel.dev>
2024-11-21 12:05:47 -0600

    Seed test db in pytest fixture

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -4,50 +4,20 @@ from sqlalchemy.orm import sessionmaker
 from ..main import app
 from ..database import get_db, create_test_engine
 from ..models import metadata_obj
+from .seed_test_db import seed_test_db
 
 # create test database engine and session
 TestEngine = create_test_engine()
 TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=TestEngine)
 
-# @pytest.fixture()
-# def test_db():
-#     """
-#     Set up, create session, and tear down test database
-#     """
-#     metadata_obj.create_all(bind=TestEngine)
-#     try:
-#         # yield sessionmaker for tests
-#         yield TestSessionLocal
-#     finally:
-#         metadata_obj.drop_all(bind=TestEngine)
-
-# # Override the get_db dependency to use the test database
-# @pytest.fixture()
-# def override_get_db(test_db):
-#     """
-#     Override `get_db` dependency to use test database
-#     """
-#     def _override_get_db():
-#         print("Overriding get_db dependency")
-#         db = test_db()
-#         try:
-#             yield db
-#         finally:
-#             db.close()
-
-#     app.dependency_overrides[get_db] = _override_get_db
-#     yield
-#     app.dependency_overrides.clear()
-
-# @pytest.fixture()
-# def test_client():
-#     with TestClient(app) as client:
-#         yield client
 
 @pytest.fixture(scope='session')
 def test_db():
     # initialize database
     metadata_obj.create_all(bind=TestEngine)
+
+    # seed database
+    seed_test_db(TestSessionLocal)
 
     # override app dependency
     def _override_get_db():
---
+++ api/tests/seed_test_db.py
@@ -0,0 +1,105 @@
+from datetime import datetime 
+from api.models import User, Recipe, Tag, recipe_tag, Unit, Ingredient, Direction, complementary_dish
+from sqlalchemy.orm import sessionmaker  # for typing
+import csv
+from pathlib import Path
+
+DIR_SEED_DATA = Path(__file__).parents[2] / 'seed_data'
+
+seed_map = [
+    {
+        'name': 'user',
+        'cls': User,
+        'file': 'data_user.csv',
+        'dt_cols': [],
+        'float_cols': [],
+    },
+    {
+        'name': 'recipe',
+        'cls': Recipe,
+        'file': 'data_recipe.csv',
+        'dt_cols': ['date_created', 'date_modified'],
+        'float_cols': [],
+    },
+    {
+        'name': 'tag',
+        'cls': Tag,
+        'file': 'data_tag.csv',
+        'dt_cols': [],
+        'float_cols': [],
+    },
+    {
+        'name': 'unit',
+        'cls': Unit,
+        'file': 'data_unit.csv',
+        'dt_cols': [],
+        'float_cols': [],
+    },
+    {
+        'name': 'ingredient',
+        'cls': Ingredient,
+        'file': 'data_ingredient.csv',
+        'dt_cols': [],
+        'float_cols': ['quantity'],
+    },
+    {
+        'name': 'direction',
+        'cls': Direction,
+        'file': 'data_direction.csv',
+        'dt_cols': [],
+        'float_cols': [],
+    },
+]
+
+seed_map_assoc = [
+    {
+        'name': 'recipe_tag',
+        'tbl': recipe_tag,
+        'file': 'data_recipetag.csv',
+    },
+    {
+        'name': 'complementary_dish',
+        'tbl': complementary_dish,
+        'file': 'data_complementarydish.csv',
+    },
+]
+def seed_test_db(session_maker: sessionmaker):
+    with session_maker() as db:
+        # models
+        for mapper in seed_map:
+            print(mapper['name'])
+            mod_inst_items = []
+
+            with open(DIR_SEED_DATA / f'{mapper["file"]}', newline='\n') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print(row)
+                    mod_inst = mapper['cls']()
+                    for key, value in row.items():
+                        if key == 'id':  # let postgres handle the id creation
+                            continue
+                        if value == '':  # overwrite empty value with None (null)
+                            value = None
+                        if key in mapper['dt_cols'] and value != None:
+                            value = datetime.strptime(value, '%Y-%m-%d')
+                        setattr(mod_inst, key, value)
+                    mod_inst_items.append(mod_inst)
+
+            for mod_inst in mod_inst_items:
+                db.add(mod_inst)
+        
+        db.commit()
+
+        # association tables
+        for mapper in seed_map_assoc:
+            print(mapper['name'])
+            records = []
+            with open(DIR_SEED_DATA / f'{mapper["file"]}', newline='\n') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print(row)
+                    records.append(row)
+            multiple_insert = mapper['tbl'].insert().values(records)
+            db.execute(multiple_insert)
+
+        db.commit()
